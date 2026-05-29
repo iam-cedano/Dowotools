6f39287b90a9ea79a323d9254717f721a5ebbe4c
Oscar Cedano <cedano@engineer.com>
2026-05-29 16:07:39 -0600

    feat: new languages

--- src/app/Language/en/Header.php
+++ src/app/Language/en/Header.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+<<<<<<< HEAD
     'page-title' => 'Dowotools - Variety Tools. For Free. Always',
     'search-placeholder' => 'Search tools, guides, and more...',
+=======
+    'page-title' => 'Dowotools - Variety Tools. For Free. Always'
+>>>>>>> 9954f4291fe01f15c005a23208238c102c43ea0a
 ];  
--- src/app/Language/es/Header.php
+++ src/app/Language/es/Header.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+<<<<<<< HEAD
     'page-title' => 'Dowotools - Herramientas de Variedad. Gratis. Siempre',
     'search-placeholder' => 'Buscar herramientas, guías y más...',
+=======
+    'page-title' => 'Dowotools - Herramientas de Variedad. Gratis. Siempre'
+>>>>>>> 9954f4291fe01f15c005a23208238c102c43ea0a
 ];
