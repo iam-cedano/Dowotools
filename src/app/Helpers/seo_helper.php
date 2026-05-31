3bff40538dc368e6d94d40977e2b4ca19de935b3
Oscar Cedano <cedano@engineer.com>
2026-05-30 18:51:45 -0600

    feat: creating seo and header helper to print metatags

---
+++ src/app/Helpers/seo_helper.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!function_exists('render_localization_meta_tags')) {
+    function render_localization_meta_tags()
+    {
+        $supportedLocales = config('App')->supportedLocales;
+
+        $currentUri = current_url(true);
+
+        foreach ($supportedLocales as $locale) {
+            $uriClone = clone $currentUri;
+
+            $uriClone->setSegment(1, $locale);
+
+            $url = (string) $uriClone;
+            ?>
+                <link rel="alternate" hreflang="<?= $locale ?>" href="<?= $url ?>" />
+            <?php
+        }
+    }
+}
